d85e8bae8cb8ff4137c284e91f44ce1e6542b460
João Lessa <joaovictorlessa98@gmail.com>
2023-09-01 12:13:03 -0300

    Fuc delete, interface tasks. Estilização btn/inpt

--- app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
+++ app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+//interface
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
+
+
+  public taskList: Array<TaskList> = [
+    {task: "Minha nova Task", checked: true},
+    {task: "Minha nova Task2", checked: false}
+  ];
+
+
+  constructor(){}
+  ngOnInit(): void {
+      
+  }
+
+  public deleteItemTaskList(event:number){
+    this.taskList.splice(event,1)
+  }
+
+  public deleteAllTaskList(){
+    const confirm = window.confirm("Você deseja realmente deletar tudo?");
+
+    if(confirm){
+      this.taskList = []
+    }
+
+    
+  }
 
 }
